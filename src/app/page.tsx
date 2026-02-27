b250e4e8c776a4af7bf228f10e85b933f996ae03
vpcosta <vpcosta258@gmail.com>
2026-02-26 21:03:26 -0300

    Criacao do card mais formulário de adição de tarefa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,43 @@
-const Home = () => {
-    return (
-        <h2>Teste</h2>
-    );
-}
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { List, ListChecks, ListX, Plus } from "lucide-react";
 
-export default Home;
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center w-full h-screen bg-rose-50">
+      <Card className="w-lg">
+        <CardHeader className="flex gap-2">
+          <Input placeholder="Criar nova tarefa" />
+          <Button variant="default" className="cursor-pointer">
+            <Plus />
+            Adicionar Tarefa
+          </Button>
+        </CardHeader>
+
+        <CardContent>
+          <Separator />
+
+          <div className="mt-5 flex gap-2">
+            <Badge className="cursor-pointer">
+              <List />
+              Todas
+            </Badge>
+
+            <Badge className="cursor-pointer">
+              <ListChecks />
+              Concluídas
+            </Badge>
+
+            <Badge className="cursor-pointer">
+              <ListX />
+              Não Concluídas
+            </Badge>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
